5d55ddc9e55833b4dbcf8a1367645e50a013a5a9
JacobDChamberlain <straightlegmusic@gmail.com>
2024-04-21 01:58:08 -0500

    progress on mobile spacing

--- src/components/Pages/Shows/Shows.js
+++ src/components/Pages/Shows/Shows.js
@@ -7,17 +7,15 @@ export default function Shows() {
             <h2 className='shows-page-header'>2024 Dates:</h2>
             <ul className='show-dates-ul'>
                 <li className='show-date-li'>
-                    <h3>May 28th 2024 @ Treebomb Park</h3>
+                    <h3>May 28th 2024 @ DoubleWide</h3>
                     <span>
-                        description about venue and<br/>
-                        other bands playing that night
+                        With ASCENDED DEAD
                     </span>
                 </li>
                 <li className='show-date-li'>
-                    <h3>May 31st 2024 @ Rubber Boots</h3>
+                    <h3>May 31st 2024 @ Rubber Gloves</h3>
                     <span>
-                        otherdescription about different venue<br/>
-                        and even sicker bands on the lineup
+                        With Herakleion
                     </span>
                 </li>
             </ul>
