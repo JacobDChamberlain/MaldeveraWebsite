6b2e827274fb278281f5efca016f328dda9a0a81
JacobDChamberlain <straightlegmusic@gmail.com>
2024-04-28 00:33:55 -0500

    potential border style

--- src/components/Pages/Shows/Shows.js
+++ src/components/Pages/Shows/Shows.js
@@ -7,18 +7,18 @@ export default function Shows() {
             <h2 className='shows-page-header'>Upcoming Shows:</h2>
             <ul className='show-dates-ul'>
                 <li className='show-date-li'>
-                    <span>
+                    <div className='show-date-description'>
                         May 28th, 2024<br />
                         @ DoubleWide<br />
                         w/ ASCENDED DEAD
-                    </span>
+                    </div>
                 </li>
                 <li className='show-date-li'>
-                    <span>
+                    <div className='show-date-description'>
                         May 31st, 2024<br />
                         @ Rubber Gloves<br />
                         w/ HERAKLEION
-                    </span>
+                    </div>
                 </li>
             </ul>
         </div>
