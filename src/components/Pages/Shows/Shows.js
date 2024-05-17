049a3baa86808018af36ae7df563ff7c9073f324
JacobDChamberlain <straightlegmusic@gmail.com>
2024-05-16 23:11:11 -0500

    add flyers & styling for upcoming shows

--- src/components/Pages/Shows/Shows.js
+++ src/components/Pages/Shows/Shows.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import './Shows.css';
 
+import may28th from './Flyers/may28thDWshow.jpg';
+import may31st from './Flyers/may31RGshow.png';
+
 export default function Shows() {
     return (
         <div className="shows-wrapper">
             <h2 className='shows-page-header'>Upcoming Shows:</h2>
             <ul className='show-dates-ul'>
                 <li className='show-date-li'>
-                    <span>
+                    <img className='show-flyer' src={ may28th } alt='may_28th_2024_at_Doublewide'></img>
+                    <div className='show-description'>
                         May 28th, 2024<br />
                         @ DoubleWide<br />
                         w/ ASCENDED DEAD
-                    </span>
+                    </div>
                 </li>
                 <li className='show-date-li'>
-                    <span>
+                <img className='show-flyer' src={ may31st } alt='may_31st_2024_at_RubberGloves'></img>
+                    <div className='show-description'>
                         May 31st, 2024<br />
                         @ Rubber Gloves<br />
                         w/ HERAKLEION
-                    </span>
+                    </div>
                 </li>
             </ul>
         </div>
