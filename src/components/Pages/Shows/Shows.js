98f6ff5b9b301a9e3f41cde1edf24908a0c51af7
JacobDChamberlain <straightlegmusic@gmail.com>
2024-08-27 03:31:01 -0500

    this will work

--- src/components/Pages/Shows/Shows.js
+++ src/components/Pages/Shows/Shows.js
@@ -19,17 +19,17 @@ export default function Shows() {
     //? have last band be preceded by '&'
 
     const shows = [
-        { flyer: may28th, alt: 'may_28th_2024_DoubleWide' },
-        { flyer: may31st, alt: 'may_31st_2024_RubberGloves' },
-        { flyer: june4th, alt: 'june_4th_2024_SunshineBar' },
-        { flyer: june22nd, alt: 'june_22nd_2024_DoubleWide' },
-        { flyer: june27th, alt: 'june_27th_2024_HaltomTheater' },
-        { flyer: june30th, alt: 'june_30th_2024_CharliesStarLounge' },
-        { flyer: august16h, alt: 'august_16th_2024_DoubleWide' },
-        { flyer: sept14th, alt: 'sept_14th_2024_GrowlRecords' },
-        { flyer: fm2m, alt: 'sept_27th_2024_ZoundsBSide' },
-        { flyer: fm2m, alt: 'october_13th_2024_DoubleWide' },
-        { flyer: fm2m, alt: 'december_7th_2024_RenosChopShop' }
+        { flyer: '.'+may28th, alt: 'may_28th_2024_DoubleWide' },
+        { flyer: '.'+may31st, alt: 'may_31st_2024_RubberGloves' },
+        { flyer: '.'+june4th, alt: 'june_4th_2024_SunshineBar' },
+        { flyer: '.'+june22nd, alt: 'june_22nd_2024_DoubleWide' },
+        { flyer: '.'+june27th, alt: 'june_27th_2024_HaltomTheater' },
+        { flyer: '.'+june30th, alt: 'june_30th_2024_CharliesStarLounge' },
+        { flyer: '.'+august16h, alt: 'august_16th_2024_DoubleWide' },
+        { flyer: '.'+sept14th, alt: 'sept_14th_2024_GrowlRecords' },
+        { flyer: '.'+fm2m, alt: 'sept_27th_2024_ZoundsBSide' },
+        { flyer: '.'+fm2m, alt: 'october_13th_2024_DoubleWide' },
+        { flyer: '.'+fm2m, alt: 'december_7th_2024_RenosChopShop' }
     ];
 
     return (
