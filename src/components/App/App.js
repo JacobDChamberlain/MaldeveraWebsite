95af3208b7b2368f2a14993816d0c9817342ab80
JacobDChamberlain <straightlegmusic@gmail.com>
2024-08-31 17:53:23 -0500

    add fixed footer container component for audio player and space on all pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import NavBar from '../NavBar/NavBar';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import SocialMediaBar from '../SocialMediaBar/SocialMediaBar';
 import Home from '../Pages/Home/Home';
 import Merch from '../Pages/Merch/Merch';
@@ -19,20 +20,21 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavBar />
-      <Header />
-      <SocialMediaBar />
-        <Routes>
-          <Route path='/' Component={ Home } />
-          <Route path='/merch' Component={ Merch } />
-          <Route path='/about' Component={ About } />
-          <Route path='/shows' Component={ Shows } />
-          <Route path='/tours' Component={ Tours } />
-          <Route path='/contact' Component={ Contact } />
-          <Route path='/giveusyourmoney' Component={ Purchase } />
-          <Route path='*' Component={ NotFound } />
-        </Routes>
+        <NavBar />
+        <Header />
+        <SocialMediaBar />
+          <Routes>
+            <Route path='/' Component={ Home } />
+            <Route path='/merch' Component={ Merch } />
+            <Route path='/about' Component={ About } />
+            <Route path='/shows' Component={ Shows } />
+            <Route path='/tours' Component={ Tours } />
+            <Route path='/contact' Component={ Contact } />
+            <Route path='/giveusyourmoney' Component={ Purchase } />
+            <Route path='*' Component={ NotFound } />
+          </Routes>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
