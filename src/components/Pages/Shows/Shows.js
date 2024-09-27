f0714172ec2b69c546c9ae10d930cedbf3040950
JacobDChamberlain <straightlegmusic@gmail.com>
2024-09-26 23:11:39 -0500

    update addy

--- src/components/Pages/Shows/Shows.js
+++ src/components/Pages/Shows/Shows.js
@@ -32,7 +32,7 @@ export default function Shows() {
         // { flyer: '.'+sept14th, alt: 'sept_14th_2024_GrowlRecords', date: 'September 14th, 2024', venue: 'Growl Records', address: '09 E Abram St, Arlington, TX 76010', bands: ['Festival'] },
         { flyer: '.'+sept27th, alt: 'sept_27th_2024_ZoundsBSide', date: 'September 27th, 2024', venue: 'Zounds B-Side', address: '10050 Shoreview Rd, Dallas, TX 75238', bands: ['Festival'] },
         { flyer: '.'+oct4, alt: 'october_4th_2024_Renos', date: 'October 4th, 2024', venue: 'Renos', address: '210 N Crowdus St, Dallas, TX 75226', bands: ['In Human Form', 'Mouth of Cronus', 'Norman Invasion'] },
-        { flyer: '.'+oct13, alt: 'october_13th_2024_DoubleWide', date: 'October 13th, 2024', venue: 'DoubleWide', address: '3510 Commerce St, Dallas, TX', bands: ['Civil Serpents'] },
+        { flyer: '.'+oct13, alt: 'october_13th_2024_GrowlRecords', date: 'October 13th, 2024', venue: 'Growl Records', address: '09 E Abram St, Arlington, TX 76010', bands: ['Civil Serpents', 'Negative Influence', 'Bliss Fields'] },
         { flyer: '.'+fm2m, alt: 'december_7th_2024_RenosChopShop', date: 'December 7th, 2024', venue: 'Reno\'s Chop Shop', address: '210 N Crowdus St, Dallas, TX 75226', bands: ['Festival'] }
     ];
 
