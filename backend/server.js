7b4548b9632e0f44b113b82d93fa61fd7079b500
JacobDChamberlain <straightlegmusic@gmail.com>
2024-10-30 14:15:48 -0500

    revert dockerfile changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { sequelize, Item } = require('./models'); // Import Sequelize instance and Item model
 const app = express();
-require('dotenv').config();
+// require('dotenv').config();
 const port = process.env.PORT || 5001;
 
 
