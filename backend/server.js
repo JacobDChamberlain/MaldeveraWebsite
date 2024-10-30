96141dc17508c8d38c35a494aba1b9758d714f76
JacobDChamberlain <straightlegmusic@gmail.com>
2024-10-30 12:06:37 -0500

    add render host to config && update port on server

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { sequelize, Item } = require('./models'); // Import Sequelize instance and Item model
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors());
